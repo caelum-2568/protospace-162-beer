247f12331597b7d922eb065422f112a79aaad244
caelum-2568 <152895310+caelum-2568@users.noreply.github.com>
2023-12-25 19:13:52 +0900

    プロトタイプ投稿機能 実装7

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources :prototypes do
-    
-  end
   resources :prototypes, only: [:index, :new, :create]
 end
