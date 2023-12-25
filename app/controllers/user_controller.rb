e1080e94a0553db4d31facfaf0c709becd946784
caelum-2568 <152895310+caelum-2568@users.noreply.github.com>
2023-12-25 19:54:44 +0900

    プロトタイプ 投稿機能 実装11

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,5 @@
 class UserController < ApplicationController
   #下記2点いつかはは使うと思う
-  #has_many :prototype #prototypes テーブルとのアソシエーション
+  has_many :prototypes #prototypes テーブルとのアソシエーション
   #has_many :comment #comments テーブルとのアソシエーション
 end
