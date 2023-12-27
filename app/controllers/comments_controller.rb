f577c32e85ea800d4ccf549f573cff57805452b3
flcamp0217 <fl.it.72183@gmail.com>
2023-12-27 16:54:34 +0900

    プルリクしたい！

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,7 @@ class CommentsController < ApplicationController
       render "prototypes/show" # views/prototypes/show.html.erbのファイルを参照しています。
     end
   end
+  
   def show
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
