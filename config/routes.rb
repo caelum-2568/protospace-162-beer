174c81263ea4cbe2829577fbc6251b2b31b44b40
caelum-2568 <152895310+caelum-2568@users.noreply.github.com>
2023-12-27 10:31:46 +0900

    プロトタイプ編集機能 実装1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
+<<<<<<< Updated upstream
   resources :prototypes, only: [:index, :new, :create, :edit, :update]
+=======
+  resources :prototypes#, only: [:index, :new, :create]
+  resources :users, only: :show
+>>>>>>> Stashed changes
 end
