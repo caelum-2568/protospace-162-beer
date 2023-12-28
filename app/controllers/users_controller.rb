d5e3037b484da848a33153a0617f2a8f53ed85f9
caelum-2568 <152895310+caelum-2568@users.noreply.github.com>
2023-12-28 11:34:48 +0900

    ユーザー詳細ページ機能 実装2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,11 @@ class UsersController < ApplicationController
   def show 
     #@users = User.includes(:user)
     #@user = User.find(params[:id])
-    user = User.find(params[:id])
-    @name = user.name
-    @profile = user.profile
-    @occupation = user.occupation
-    @position = user.position
+    @user = User.find(params[:id])
+    @prototypes = @user.prototypes
+    #@name = user.name
+    #@profile = user.profile
+    #@occupation = user.occupation
+    #@position = user.position
   end
 end
